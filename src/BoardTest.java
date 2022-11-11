3141eb39b77f11bf4725a64d0ed1fc0543abb22b
NIU1603749 <101998724+elisendagc@users.noreply.github.com>
2022-11-12 00:25:08 +0100

    Fix and add tests

--- src/BoardTest.java
+++ src/BoardTest.java
@@ -2,9 +2,12 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+//import Board.cell;
+
 
 
 public class BoardTest {
+	
 	@Test
 	public void testBoard() {
 		Board board = new Board();
@@ -13,15 +16,16 @@ public class BoardTest {
 				assertEquals(board.getBoard()[i][j], Board.cell.water);
 			}
 		}
-		
 	}
+	
+	
 	@Test
 	public void testConvertLetter() {
 		Board board = new Board();
-		String lletra = "A1";
+		String lletra = "B1";
 		int[] pos = new int[2];
-		pos[0]=0;
-		pos[1]=0;
+		pos[0]=1; // leter
+		pos[1]=0; // number
 		assertArrayEquals(board.convertStringToPosition(lletra), pos);
 	}
 	
@@ -38,19 +42,83 @@ public class BoardTest {
 		String lletra = "B1";
 		boolean orientation = false;
 		int length = 3;
-	    	board.insertBoat(lletra, orientation, length);
-	    	assertEquals(board.getBoard()[1][0], Board.cell.boat); // B1
-	    	assertEquals(board.getBoard()[2][0], Board.cell.boat); // C1
-	   	 assertEquals(board.getBoard()[3][0], Board.cell.boat); // D1
+	    board.insertBoat(lletra, orientation, length);
+	    assertEquals(board.getBoard()[0][1], Board.cell.boat); // B1
+	    assertEquals(board.getBoard()[1][1], Board.cell.boat); // B2
+	    assertEquals(board.getBoard()[2][1], Board.cell.boat); // B3
 	}
 	
 	@Test
 	public void testHorizontalInsertBoat() {
 		Board board = new Board();
 	    board.insertBoat("B1", true, 3); // insert in B1 a lenght 3 horizontal boat
-	    assertEquals(board.getBoard()[1][0], Board.cell.boat); // B1
-	    assertEquals(board.getBoard()[1][1], Board.cell.boat); // B2
-	    assertEquals(board.getBoard()[1][2], Board.cell.boat); // B3
+	    assertEquals(board.getBoard()[0][1], Board.cell.boat); // B1
+	    assertEquals(board.getBoard()[0][2], Board.cell.boat); // C1
+	    assertEquals(board.getBoard()[0][3], Board.cell.boat); // D1
+	}
+	
+	
+	@Test
+	public void testHorizontalInsertBoatLimitRight() {
+		Board board = new Board();
+	    assertEquals(board.insertBoat("F1", true, 3), true);
+	}
+	
+	@Test
+	public void testHorizontalInsertBoatLimitRightInvalid() {
+		Board board = new Board();
+	    assertEquals(board.insertBoat("G1", true, 3), false);
+	}
+	
+	@Test
+	public void testHorizontalInsertBoatLimitBottom() {
+		Board board = new Board();
+	    assertEquals(board.insertBoat("F8", true, 3), true);
+	}
+	
+	@Test
+	public void testVerticalInsertBoatLimitRight() {
+		Board board = new Board();
+	    assertEquals(board.insertBoat("H1", false, 3), true);
+	}
+	
+	@Test
+	public void testVerticalInsertBoatLimitBottom() {
+		Board board = new Board();
+	    assertEquals(board.insertBoat("H6", false, 3), true);
+	}
+	
+	@Test
+	public void testVerticalInsertBoatLimitBottomInvalid() {
+		Board board = new Board();
+	    assertEquals(board.insertBoat("H7", false, 3), false);
+	}
+	
+	
+	@Test
+	public void testBuildBoard() {
+		
+		Boat[] boats = new Boat[3];
+		Boat b0 = new Boat(2);
+		boats[0] = b0; // position = A2 H
+		Boat b1 = new Boat(3);
+		boats[1] = b1; // position = E3 V
+ 		Boat b2 = new Boat(3);
+		boats[2] = b2; // position H6 V
+		Board board = new Board();
+		board.setBoats(boats);
+		board.buildBoard();
+		assertEquals(board.getBoard()[1][0], Board.cell.boat); // A2
+		assertEquals(board.getBoard()[1][1], Board.cell.boat); // B2
+		
+		assertEquals(board.getBoard()[2][4], Board.cell.boat); // E3
+		assertEquals(board.getBoard()[3][4], Board.cell.boat); // E4
+		assertEquals(board.getBoard()[4][4], Board.cell.boat); // E5
+		
+
+		assertEquals(board.getBoard()[5][7], Board.cell.boat); // H6
+		assertEquals(board.getBoard()[6][7], Board.cell.boat); // H7
+		assertEquals(board.getBoard()[7][7], Board.cell.boat); // H8
 	}
 	
 	@Test
@@ -66,9 +134,9 @@ public class BoardTest {
 		Board board = new Board();
 		board.insertBoat("B1", false, 3);
 		board.makeMoveHit("B1");
-		assertEquals(board.getBoard()[1][0], Board.cell.hit);
+		assertEquals(board.getBoard()[0][1], Board.cell.hit);
 	}
-	
+	 
 	@Test
 	public void testWinner() {
 		Board board = new Board();
