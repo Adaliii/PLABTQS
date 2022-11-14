2cb7267379f786b25d9a442861fb494dd0557749
NIU1603749 <101998724+elisendagc@users.noreply.github.com>
2022-11-15 00:03:31 +0100

    add comments

--- src/ControllerGame.java
+++ src/ControllerGame.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
 
+/**
+ * La classe Controllergame es l'encargada de controlar la interacció entre la vista i el model del Game
+ */
 public class ControllerGame {
 	
 	public Player players[] = new Player[2];
@@ -9,7 +12,9 @@ public class ControllerGame {
 	public ModelDatabase DB = new ModelDatabase("./Ranking.txt");
 	public ViewBoard viewBoard = new ViewBoard();
 	 
-	//Creates players
+	/**
+   	* Inicialitza els atributs de cada jugador.
+ 	*/
 	public void setUpPlayers() {
 		int i = 0;
 		for(Player p: players) {
@@ -27,13 +32,21 @@ public class ControllerGame {
 			}
 		}
 	}
+	
+	/**
+   	* Inicialitza el tauler de cada jugador, cada un col·loca els seus barcos.
+ 	*/
 	public void setUpBoards() {
 		for (Player p: players) {
 			p.getBoard().buildBoard();
 			viewBoard.showBoardToPlayer(p.getBoard());
 		}
 	}
-	//main flow of the game
+	/**
+   	* Es el flux principal del joc, a través de crides al tauler i altres classes cada jugador fa els seus moviments fins que un guanya.
+	*
+ 	* @return    el jugador guanyador
+ 	*/
 	public Player makeGame() throws IOException {
 		setUpBoards();
 		Player actualPlayer = players[0];
@@ -58,6 +71,11 @@ public class ControllerGame {
 		return actualPlayer;
 	}
 	
+	/**
+   	* Actualitza/guarda  en la base de dades del ranking el jugador que hagi guanyat en la partida.
+	*
+	* @param  p	jugador que ha guanyat una partida
+ 	*/
 	public void winner(Player p) throws IOException {
 		viewPlayer.showWinner(p);
 		DB.connect();
