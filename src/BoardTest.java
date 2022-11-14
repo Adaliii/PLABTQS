34401f7a3f18b14458f8c22f4612dda726c4e543
NIU1603749 <101998724+elisendagc@users.noreply.github.com>
2022-11-14 23:14:47 +0100

    Add coments

--- src/BoardTest.java
+++ src/BoardTest.java
@@ -1,22 +1,19 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-//import Board.cell;
-
-
-
 public class BoardTest {
+	// Test del constructor, comprova que inicialitzi correctament tot el tauler amb aigua.
 	@Test
-    public void testBoard() {
-        Board board = new Board();
-        for(int i = 0; i<8;i++) {
-            for(int j=0;j<8;j++) {
-                assertTrue(board.getBoard()[i][j].equals(Board.cell.water));
-            }
-        }
-    }
+	public void testBoard() { 
+		Board board = new Board();
+		for(int i = 0; i<8;i++) {
+		    for(int j=0;j<8;j++) {
+			assertTrue(board.getBoard()[i][j].equals(Board.cell.water));
+		    }
+		}
+	}
 	
+	// Test que pasa posició string a array int, comprova que retorni la posició esperada.
 	@Test
 	public void testConvertLetter() {
 		Board board = new Board();
@@ -27,6 +24,7 @@ public class BoardTest {
 		assertArrayEquals(board.convertStringToPosition(lletra), pos);
 	}
 	
+	// Test de getter i setter de les dimensions, comprova que es retorni el resultat esperat.
 	@Test
 	public void testGetSetDimention() {
 		Board board = new Board();
@@ -34,45 +32,56 @@ public class BoardTest {
 		assertEquals(board.getDimention(), 3);
 	}
 	
+	// Test de l'inserció de barcos, insereix varis barcos i comprova si s'han col·locat correctament en el tauler.
+	// Comprova situacions invalides de sobreposicio de barcos.
+	// Fa proves de caixa negra on es comprova:
+	//	- Partició Equivalent.
+	// També realitza les proves de caixa blanca:
+	// 	- Statement coverage, decision coverage, condition coverage.
 	@Test
 	public void testInsertBoat() {
 		Board board = new Board();
 		String lletra = "B1";
 		boolean orientation = false;
 		int length = 3;
-	    board.insertBoat(lletra, orientation, length);
-	    assertEquals(board.getBoard()[0][1], Board.cell.boat); // B1
-	    assertEquals(board.getBoard()[1][1], Board.cell.boat); // B2
-	    assertEquals(board.getBoard()[2][1], Board.cell.boat); // B3
-	    assertFalse(board.insertBoat(lletra, orientation, length));
+	    	board.insertBoat(lletra, orientation, length);
+	    	assertEquals(board.getBoard()[0][1], Board.cell.boat); // B1
+	    	assertEquals(board.getBoard()[1][1], Board.cell.boat); // B2
+	    	assertEquals(board.getBoard()[2][1], Board.cell.boat); // B3
+	    	assertFalse(board.insertBoat(lletra, orientation, length));
 		Board board1 = new Board();
-	    board1.insertBoat("B1", true, 3); // insert in B1 a length 3 horizontal boat
-	    assertEquals(board1.getBoard()[0][1], Board.cell.boat); // B1
-	    assertEquals(board1.getBoard()[0][2], Board.cell.boat); // C1
-	    assertEquals(board1.getBoard()[0][3], Board.cell.boat); // D1
+	    	board1.insertBoat("B1", true, 3); // insereix en B1 un barco horitzontal de llargada 3
+	    	assertEquals(board1.getBoard()[0][1], Board.cell.boat); // B1
+	    	assertEquals(board1.getBoard()[0][2], Board.cell.boat); // C1
+	    	assertEquals(board1.getBoard()[0][3], Board.cell.boat); // D1
 	    
-	    assertFalse(board1.insertBoat("B1", true, 3)); //Already inserted Boat
+	    	assertFalse(board1.insertBoat("B1", true, 3)); // barco ja inserit anteriorment
 	    
 		Board board2 = new Board();
-	    assertEquals(board2.insertBoat("F1", true, 3), true);
+	    	assertEquals(board2.insertBoat("F1", true, 3), true);
 	
 		Board board3 = new Board();
-	    assertEquals(board3.insertBoat("G1", true, 3), false);
+	    	assertEquals(board3.insertBoat("G1", true, 3), false);
 	
 		Board board4 = new Board();
-	    assertEquals(board4.insertBoat("F8", true, 3), true);
+	    	assertEquals(board4.insertBoat("F8", true, 3), true);
 	
 		Board board5 = new Board();
-	    assertEquals(board5.insertBoat("H1", false, 3), true);
+	    	assertEquals(board5.insertBoat("H1", false, 3), true);
 	
 		Board board6 = new Board();
-	    assertEquals(board6.insertBoat("H6", false, 3), true);
+	    	assertEquals(board6.insertBoat("H6", false, 3), true);
 	
 		Board board7 = new Board();
-	    assertEquals(board7.insertBoat("H7", false, 3), false);
+	    	assertEquals(board7.insertBoat("H7", false, 3), false);
 	}
 	
-	
+	// Test de la contrucció tauler a través de inserir barcos en varies posicions, invalides i correctes.
+	// Aquestes posicions les rep del Mock de ViewBoard, que simulen posibles inputs dels jugadors.
+	// Fa proves de caixa negra on es comprova:
+	//	- Partició Equivalent, valors límit.
+	// També realitza les proves de caixa blanca:
+	// 	- Statement coverage, decision coverage, condition coverage.
 	@Test
 	public void testBuildBoard() {
 		
@@ -136,7 +145,7 @@ public class BoardTest {
 		
 		Boat[] boats2 = new Boat[1];
 		Boat b02 = new Boat(2);
-		boats2[0] = b02; // position = H8 V and later H7 V
+		boats2[0] = b02; // position = H8 V i després H7 V
 		Board board2 = new Board();
 		board2.setBoats(boats2);
 		MockViewBoard mBoard2 = new MockViewBoard();
@@ -152,7 +161,7 @@ public class BoardTest {
 		
 		Boat[] boats3 = new Boat[1];
 		Boat b03 = new Boat(2);
-		boats3[0] = b03; // position = H8 H and later G8 H
+		boats3[0] = b03; // position = H8 H i després G8 H
 		Board board3 = new Board();
 		board3.setBoats(boats3);
 		MockViewBoard mBoard3 = new MockViewBoard();
@@ -166,12 +175,16 @@ public class BoardTest {
 		sb3.showBoardToPlayer(board3);
 	}
 	
+	// Test del moviment del jugador, fa diferents moviments vàlids i invalids sobre el tauler.
+	// Comprova que repetir un moviment en una mateixa posició no afecti al tauler.
+	// Fa proves de caixa blanca:
+	// 	- Statement coverage, decision coverage, condition coverage.
 	@Test
 	public void testMakeMove() {
 		Board board = new Board();
 		board.insertBoat("B1", true, 3);
 		assertEquals(board.makeMoveHit("A1"), 1);
-		assertEquals(board.makeMoveHit("A1"), 3); // already miss
+		assertEquals(board.makeMoveHit("A1"), 3); // ja ha fallat
 		ViewBoard sb = new ViewBoard();
 		sb.showBoardToEnemy(board);
 		assertEquals(board.getBoard()[0][0], Board.cell.miss);
@@ -179,12 +192,15 @@ public class BoardTest {
 		Board board1 = new Board();
 		board1.insertBoat("B1", false, 3);
 		assertEquals(board1.makeMoveHit("B1"), 0);
-		assertEquals(board1.makeMoveHit("B1"), 2); // already hit
+		assertEquals(board1.makeMoveHit("B1"), 2); // ja li ha adonat
 		ViewBoard sb1 = new ViewBoard();
 		sb.showBoardToEnemy(board1);
 		assertEquals(board1.getBoard()[0][1], Board.cell.hit);
 	}
-	 
+	
+	// Test per mirar si hi ha guanyador, comprova que si es derriben tots els barcos el jugador guanyi i viceversa.
+	// Fa proves de caixa blanca:
+	// 	- Statement coverage, decision coverage, condition coverage.
 	@Test
 	public void testWinner() {
 		Board board = new Board();
